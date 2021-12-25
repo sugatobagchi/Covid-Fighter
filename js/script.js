19ba6b72648c3f32f349bb7b9c661aa9651e76a5
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2021-12-25 17:48:34 +0530

    fixed styling. Removed Comments

--- js/script.js
+++ js/script.js
@@ -16,9 +16,6 @@ $(document).ready(function () {
       $('.scroll-top').hide();
     }
 
-    // scroll spy 
-
-
     $('section').each(function () {
 
       let height = $(this).height();
@@ -34,8 +31,6 @@ $(document).ready(function () {
     });
 
   });
-
-  // smooth scrolling
 
   $('a[href*="#"]').on('click', function (e) {
 
@@ -54,7 +49,6 @@ $(document).ready(function () {
 
 });
 
-// Step 1: Get user coordinates
 function getCoordinates() {
   let options = {
     enableHighAccuracy: true,
@@ -88,16 +82,9 @@ function getCity(coordinates) {
   xhr.open('GET', "https://us1.locationiq.com/v1/reverse.php?key=pk.820cc28083a4dcd7e2fa332ca5b9804b&lat=" + lat + "&lon=" + lng + "&format=json", true);
   xhr.send();
   xhr.onreadystatechange = processRequest;
-  // xhr.addEventListener("readystatechange", processRequest, true);
-
   function processRequest(e) {
-    // console.log(2);
     if (xhr.readyState == 4 && xhr.status == 200) {
-      // console.log(xhr.readyState)
-      // console.log(xhr.responseText);
       let response = JSON.parse(xhr.responseText);
-      // console.log(response);
-
       let city = sta[response.address.state];
       // console.log(city);
       getCases(city)
@@ -108,31 +95,13 @@ function getCity(coordinates) {
 
 function getCases(city) {
 
-
-  // let a = new XMLHttpRequest();
-  // a.open('GET', "https://api.covid19api.com/total/country/India", true);
-  // // let r = JSON.parse(https://api.covid19api.com/country/India/status/confirmed?from=2020-03-01T00:00:00Z&to=2021-12-22T00:00:00Z)
-  // a.send();
-  // a.onreadystatechange = processRequest;
-  // a.addEventListener("readystatechange", processRequest, false);
-  // function processRequest(e){
-  //     let r = JSON.parse(a.responseText);
-  //     console.log(r);
-  //     let b = r.length
-  //     console.log(b);
-  //     let cases = r[b-1].Deaths
-  //     console.log(cases);
-  // }
-
   let a = new XMLHttpRequest();
   a.open('GET', "https://api.covid19tracker.in/data/static/timeseries.min.json", true);
   a.send();
   a.onreadystatechange = getStatus;
-  // a.addEventListener("readystatechange", getStatus, false);
   function getStatus() {
     if (a.readyState == 4 && a.status == 200) {
       let r = JSON.parse(a.responseText);
-      console.log(r);
       let b = r[city];
       b = b["dates"]
       let yourDate = new Date();
@@ -142,11 +111,9 @@ function getCases(city) {
       yourDate = yourDate + "";
       D = yourDate.split(" ")
       del = b[formatDate(D[0] + " " + D[1] + " " + D[2] + " " + D[3])]
-      console.log(del.delta, del.total)
       text1 = del.delta;
       text2 = del.total;
-      console.log(text1);
-      console.log(text2);
+      
       for (let key in text1) {
         if (key == "other")
           continue;
@@ -161,29 +128,7 @@ function getCases(city) {
         head.innerHTML = key + " : " + text2[key];
         document.getElementsByClassName("unique2")[0].appendChild(head);
       }
-
-      // text1.forEach(function (ele){
-      //   let head = document.createElement("h1");
-      //   head.innerHTML = el;
-      //   document.getElementsByClassName("unique")[0].appendChild(head);
-      //   return;
-      // })
-      // text = [del.delta.confirmed, del.delta.deceased, del.delta.recovered, del.delta.vaccinated1, del.delta.vaccinated2,
-      // del.delta.tested, del.total.confirmed, del.total.deceased, del.total.recovered, del.total.vaccinated1,
-      // del.total.vaccinated2, del.total.tested,]
-      // let count = 0;
-      // text.forEach(function (el) {
-      //   // var inse = document.getElementsByClassName("unique")
-      //   let head = document.createElement("h1");
-      //   head.innerHTML = el;
-      //   document.getElementsByClassName("unique")[0].appendChild(head);
-      //   return;
-      // })
-
     }
-
-    // x += 1;
-    // } while (del == undefined || del.delta == undefined || del.delta.vaccinated1 == null || del.delta.vaccinated2 == null || del.delta.confirmed == null || del.delta.deaths == null || del.delta.deceased == null || del.delta.recorved == null || del.delta.tested == null)
   }
 }
 
@@ -201,16 +146,7 @@ function formatDate(date) {
 
   return [year, month, day].join('-');
 }
-
-
-
-
 getCoordinates();
-
-
-
-
-
 
 let sta = {
   "Andaman and Nicobar Islands": "AN",
